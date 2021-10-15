39c7fe1ada4d91c0ae10870316bcae3dc2f497eb
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2021-10-15 22:51:23 +0200

    Add VS code config, add file reading from last time, add ipynb for next practicals.

---
+++ cviko_2/read_input.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+while a != -1:
+    # write your code
+    a = int(input())
+    # write your code
+
+# write your code and remove all whitespaces around the input
+a = int(input().strip())
+print(a)
+
+print('"str".strip() examples: ')
+# '   5   ' -> '5'
+print('    5   '.strip())
+
+print('    Hello    world   '.strip())
+5 + 1
