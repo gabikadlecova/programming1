9e0bb2de377ae9265a59cabb00aaf1b8999f96b0
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2021-12-13 11:41:33 +0100

    Inheritance example.

--- cviko_11/graphs.py
+++ cviko_11/graphs.py
@@ -1,6 +1,22 @@
-class Board:
-    def __init__(self, board, diagonal_neighbors=False):
+class SimpleBoard:
+    """
+    This board does not check anything and does not understand neighbors.
+    """
+    def __init__(self, board):
         self.board = board
+
+    def get_elem(self, x, y):
+        return self.board[x][y]
+
+
+class Board(SimpleBoard):
+    """
+    This board does many cool things like checking bounds for indices or returning neighbors for an index.
+    """
+    def __init__(self, board, diagonal_neighbors=False):
+        super().__init__(board)
+
+        # specific for this board (useless in SimpleBoard)
         self.xlim = len(board)
         self.ylim = len(board[0])
         self.diagonal_neighbors = diagonal_neighbors
@@ -28,4 +44,33 @@ class Board:
         return neighbors
 
     def get_elem(self, x, y):
-        return self.board[x][y]
+        if not self.check_limits(x, y):
+            return None
+        
+        return super().get_elem(x, y)
+
+
+if __name__ == "__main__":
+    list_board = [
+        [1, 0, 1],
+        [2, 2, 1],
+        [1, 1, 1]
+    ]
+
+    print("Testing the simple board...")
+    simple_board = SimpleBoard(list_board)
+    try:
+        print(simple_board.get_elem(0, 0))
+        print(simple_board.get_elem(50, 50))  # will crash
+    except IndexError as e:
+        print("Wrong index passed to board.")
+        print(e)
+
+    print("\nTesting the extended board...")
+    not_so_simple_board = Board(list_board)
+    try:
+        print(not_so_simple_board.get_elem(0, 0))
+        print(not_so_simple_board.get_elem(50, 50))  # will NOT crash :-)
+    except IndexError as e:
+        print("Wrong index passed to board.")
+        print(e)
