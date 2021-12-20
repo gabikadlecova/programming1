e26697007384134c7c3d48cedef283230da3f462
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2021-12-20 11:36:25 +0100

    Updated bfs from tutorials.

--- cviko_11/bfs.py
+++ cviko_11/bfs.py
@@ -2,19 +2,31 @@ from graphs import Board
 from collections import deque
 
 
-def bfs(board, start_x, start_y):
-    queue = deque([(start_x, start_y)])
+def bfs(board, start_x, start_y, value_to_find=1):
+    first = {
+        'indices': (start_x, start_y),
+        'length': 0,
+        'n_ones': 0,
+        'prev': []
+    }
+
+    queue = deque([first])
     visited = set()
 
     while len(queue):
         # visit next state
-        next_elem_x, next_elem_y = queue.pop()
+        curr_dict = queue.pop()
+        next_elem_x, next_elem_y = curr_dict['indices']
+        next_length = curr_dict['length']
+
         visited.add((next_elem_x, next_elem_y))
 
         # check if it's what we're looking for
         value = board.get_elem(next_elem_x, next_elem_y)
-        if value == 1:
-            return (next_elem_x, next_elem_y)
+        if value == value_to_find:
+            return curr_dict
+        elif value == 1:
+            curr_dict['n_ones'] += 1
 
         neighbors = board.get_neighbors(next_elem_x, next_elem_y)
         # dead end
@@ -26,7 +38,13 @@ def bfs(board, start_x, start_y):
             if n in visited:
                 continue
             else:
-                queue.appendleft(n)  # enqueue
+                next_elem = {
+                    'indices': n,
+                    'length': next_length + 1,
+                    'n_ones': curr_dict['n_ones'],
+                    'prev': curr_dict['prev'].copy() + [curr_dict['indices']]
+                }
+                queue.appendleft(next_elem)  # enqueue
 
     return None
 
@@ -35,16 +53,17 @@ if __name__ == "__main__":
     test_board = [
         [0, 0, 0, 1],
         [0, 0, 0, 0],
-        [1, 0, 1, 0],
-        [0, 1, 0, 2]
+        [0, 0, 1, 0],
+        [1, 1, 0, 2]
     ]
 
     board = Board(test_board)
-    print(bfs(board, 0, 0))
+    print(bfs(board, 0, 0, value_to_find=2))
 
     board = Board(test_board, diagonal_neighbors=True)
-    print(bfs(board, 0, 0))
+    print(bfs(board, 0, 0, value_to_find=2))
 
-    # todo - add path length to queue
+    # (x) add path length to queue
     # todo - find path to 2 and count number of ones on the way
     # todo (hard exercise) - find the path to 2 with the most ones (no repeated visits on one path)
+
